c5c31d0c715cbb60ab00e8246d9f2dd8aad8c414
Sapeuri2 <Sapeuri2>
2025-04-10 12:43:08 +0100

    Première version pour production

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
-  base: 'https://github.com/Sapeuri2/vue-expense-front.git', // Assure-toi que ce chemin correspond à ton repository sur GitHub
+  base: '/vue-expense-front.git/', // Assure-toi que ce chemin correspond à ton repository sur GitHub
   build: {
     minify: 'esbuild',
     target: 'esnext',
